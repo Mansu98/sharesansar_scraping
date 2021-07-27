5424843b5b2cad290158bdd57b1df1e42cebe790
unknown <mansubajracharya09@gmail.com>
2021-07-27 20:05:26 +0545

    schedule

---
+++ index.js
@@ -0,0 +1,89 @@
+var cron = require('node-cron');
+
+cron.schedule('* * * * *', () => {
+
+const request = require("request-promise");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const json2csv = require("json2csv").Parser;
+
+
+const news = "https://www.sharesansar.com/";
+
+var newsData =[];
+(async()=>{
+    const response = await request({
+        uri:news,
+        headers:{
+            accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-encoding": "gzip, deflate, br",
+            "accept-language": "en-US,en;q=0.9"
+        },
+        gzip:true
+    });
+    let $ = cheerio.load(response);
+    $('div.col-lg-9.col-md-9.col-sm-8.col-xs-12.sp-fix > div > div.col-md-7 > div > ul > li').each((i,element)=>
+    {
+
+        const title =$(element).find('a').text().trim();
+        const link =$(element).find('a').attr('href').trim();   
+
+    newsData.push({
+      title,
+      link
+    });
+
+    (async()=>{
+        let detailData =[]; 
+        let links=[]; 
+    
+        newsData.map((item,i)=>{
+            links.push(newsData[i].link);
+            })
+     
+            for(let link of links)
+            {    
+      const response = await request({
+            uri:link, 
+              headers:{
+                accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "accept-encoding": "gzip, deflate, br",
+                "accept-language": "en-US,en;q=0.9"
+            },
+            gzip:true
+        });
+        let $ = cheerio.load(response);
+        let title= $("div > div > div.col-md-12 > h2").text().trim();
+        let date = $('div.col-md-12 > div.margin-bottom-10 > div.col-lg-8 > h5').text().trim();
+        let img = $('div > div > div > div.col-md-12 > figure > img').attr('src');
+        let detail =$('#newsdetail-content').text().trim();
+        
+      
+        detailData.push({
+            title:title,
+            date: date,
+            image: img,
+            content: detail
+           
+        });
+            }
+
+            const json2 = new json2csv();
+const csv1 = json2.parse(detailData);
+fs.writeFileSync("./detailNews.csv", csv1, "utf-8")
+})();
+
+})
+   
+const j2cp = new json2csv();
+const csv = j2cp.parse(newsData);
+fs.writeFileSync("./news.csv", csv, "utf-8")
+})
+();
+console.log("scraping done ..." );
+
+
+});
+
+
+
---
+++ try.js
@@ -0,0 +1,59 @@
+var cron = require('node-cron');
+
+cron.schedule('* * * * *', () => {
+
+var MongoClient = require('mongodb').MongoClient;
+
+// Import required module csvtojson and mongodb packages
+const csvtojson = require('csvtojson');
+const mongodb = require('mongodb');
+
+var url = "mongodb+srv://mernuser:user1@mernproject.iccbl.mongodb.net/Morntea?retryWrites=true&w=majority";
+let collectionName;
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("Morntea");
+    dbo.listCollections().toArray(function (err, collectionInfos) {
+    collectionName = collectionInfos[1].name;
+        console.log(collectionName);
+        const fileName = 'detailNews.csv';
+var arrayToInsert = [];
+csvtojson().fromFile(fileName).then(source => {
+    // Fetching the all data from each row
+    for (var i = 0; i < source.length; i++) {
+         var oneRow = {
+             title: source[i]['title'],
+             date: source[i]['date'],
+             image: source[i]['image'],
+             content: source[i]['content']
+         };
+         arrayToInsert.push(oneRow);
+     }
+    
+     var collection = dbConn.collection(collectionName);
+     collection.insertMany(arrayToInsert, (err, result) => {
+         if (err) console.log(err);
+         if(result){
+             console.log('Import CSV into database successfully.');
+         }
+     });
+});
+
+        db.close();
+    });
+});
+
+
+var dbConn;
+
+mongodb.MongoClient.connect(url, {
+  
+}).then((client) => {
+    console.log('DB Connected!');
+    dbConn = client.db();
+}).catch(err => {
+    console.log(`DB Connection Error: ${err.message}`);
+});
+
+}); //scheduled
